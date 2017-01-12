ab74d8e7c7ffcb4178054317d71f4bc1d05fab81
Xelwon <xelwon1@gmail.com>
2017-01-12 16:03:19 +0100

    added spanish language

--- src/Windows/EditReferencePaths.Designer.cs
+++ src/Windows/EditReferencePaths.Designer.cs
@@ -28,130 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonAdd = new System.Windows.Forms.Button();
-            this.buttonRemove = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.buttonCancel = new System.Windows.Forms.Button();
-            this.buttonAccept = new System.Windows.Forms.Button();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.panel2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // buttonAdd
-            // 
-            this.buttonAdd.Location = new System.Drawing.Point(0, 0);
-            this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(76, 25);
-            this.buttonAdd.TabIndex = 0;
-            this.buttonAdd.Text = "Add...";
-            this.buttonAdd.UseVisualStyleBackColor = true;
-            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
-            // 
-            // buttonRemove
-            // 
-            this.buttonRemove.Location = new System.Drawing.Point(82, 0);
-            this.buttonRemove.Name = "buttonRemove";
-            this.buttonRemove.Size = new System.Drawing.Size(76, 25);
-            this.buttonRemove.TabIndex = 1;
-            this.buttonRemove.Text = "Remove";
-            this.buttonRemove.UseVisualStyleBackColor = true;
-            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.IntegralHeight = false;
-            this.listBox1.Location = new System.Drawing.Point(8, 49);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(352, 130);
-            this.listBox1.TabIndex = 1;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
-            // panel2
-            // 
-            this.panel2.AutoSize = true;
-            this.panel2.Controls.Add(this.buttonRemove);
-            this.panel2.Controls.Add(this.buttonAdd);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(8, 19);
-            this.panel2.Name = "panel2";
-            this.panel2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
-            this.panel2.Size = new System.Drawing.Size(352, 30);
-            this.panel2.TabIndex = 2;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Controls.Add(this.panel2);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(8, 8);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(8, 6, 8, 8);
-            this.groupBox1.Size = new System.Drawing.Size(368, 187);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Directories to search for assemblies";
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.buttonAccept);
-            this.panel1.Controls.Add(this.buttonCancel);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(8, 195);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(368, 33);
-            this.panel1.TabIndex = 2;
-            // 
-            // buttonCancel
-            // 
-            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(290, 7);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
-            this.buttonCancel.TabIndex = 0;
-            this.buttonCancel.Text = "Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            // 
-            // buttonAccept
-            // 
-            this.buttonAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonAccept.Location = new System.Drawing.Point(209, 7);
-            this.buttonAccept.Name = "buttonAccept";
-            this.buttonAccept.Size = new System.Drawing.Size(75, 23);
-            this.buttonAccept.TabIndex = 0;
-            this.buttonAccept.Text = "Accept";
-            this.buttonAccept.UseVisualStyleBackColor = true;
-            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
-            // 
-            // folderBrowserDialog1
-            // 
-            this.folderBrowserDialog1.Description = "Select a directory to search for reference assemblies.";
-            // 
-            // EditReferencePaths
-            // 
-            this.AcceptButton = this.buttonAccept;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(384, 236);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.panel1);
-            this.MinimumSize = new System.Drawing.Size(300, 200);
-            this.Name = "EditReferencePaths";
-            this.Padding = new System.Windows.Forms.Padding(8);
-            this.Text = "Reference Assembly Paths";
-            this.panel2.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.ResumeLayout(false);
+        	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditReferencePaths));
+        	this.buttonAdd = new System.Windows.Forms.Button();
+        	this.buttonRemove = new System.Windows.Forms.Button();
+        	this.listBox1 = new System.Windows.Forms.ListBox();
+        	this.panel2 = new System.Windows.Forms.Panel();
+        	this.groupBox1 = new System.Windows.Forms.GroupBox();
+        	this.panel1 = new System.Windows.Forms.Panel();
+        	this.buttonAccept = new System.Windows.Forms.Button();
+        	this.buttonCancel = new System.Windows.Forms.Button();
+        	this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+        	this.panel2.SuspendLayout();
+        	this.groupBox1.SuspendLayout();
+        	this.panel1.SuspendLayout();
+        	this.SuspendLayout();
+        	// 
+        	// buttonAdd
+        	// 
+        	resources.ApplyResources(this.buttonAdd, "buttonAdd");
+        	this.buttonAdd.Name = "buttonAdd";
+        	this.buttonAdd.UseVisualStyleBackColor = true;
+        	this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+        	// 
+        	// buttonRemove
+        	// 
+        	resources.ApplyResources(this.buttonRemove, "buttonRemove");
+        	this.buttonRemove.Name = "buttonRemove";
+        	this.buttonRemove.UseVisualStyleBackColor = true;
+        	this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+        	// 
+        	// listBox1
+        	// 
+        	resources.ApplyResources(this.listBox1, "listBox1");
+        	this.listBox1.FormattingEnabled = true;
+        	this.listBox1.Name = "listBox1";
+        	this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+        	// 
+        	// panel2
+        	// 
+        	resources.ApplyResources(this.panel2, "panel2");
+        	this.panel2.Controls.Add(this.buttonRemove);
+        	this.panel2.Controls.Add(this.buttonAdd);
+        	this.panel2.Name = "panel2";
+        	// 
+        	// groupBox1
+        	// 
+        	this.groupBox1.Controls.Add(this.listBox1);
+        	this.groupBox1.Controls.Add(this.panel2);
+        	resources.ApplyResources(this.groupBox1, "groupBox1");
+        	this.groupBox1.Name = "groupBox1";
+        	this.groupBox1.TabStop = false;
+        	// 
+        	// panel1
+        	// 
+        	this.panel1.Controls.Add(this.buttonAccept);
+        	this.panel1.Controls.Add(this.buttonCancel);
+        	resources.ApplyResources(this.panel1, "panel1");
+        	this.panel1.Name = "panel1";
+        	// 
+        	// buttonAccept
+        	// 
+        	resources.ApplyResources(this.buttonAccept, "buttonAccept");
+        	this.buttonAccept.Name = "buttonAccept";
+        	this.buttonAccept.UseVisualStyleBackColor = true;
+        	this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+        	// 
+        	// buttonCancel
+        	// 
+        	resources.ApplyResources(this.buttonCancel, "buttonCancel");
+        	this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        	this.buttonCancel.Name = "buttonCancel";
+        	this.buttonCancel.UseVisualStyleBackColor = true;
+        	// 
+        	// folderBrowserDialog1
+        	// 
+        	resources.ApplyResources(this.folderBrowserDialog1, "folderBrowserDialog1");
+        	// 
+        	// EditReferencePaths
+        	// 
+        	this.AcceptButton = this.buttonAccept;
+        	resources.ApplyResources(this, "$this");
+        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        	this.CancelButton = this.buttonCancel;
+        	this.Controls.Add(this.groupBox1);
+        	this.Controls.Add(this.panel1);
+        	this.Name = "EditReferencePaths";
+        	this.panel2.ResumeLayout(false);
+        	this.groupBox1.ResumeLayout(false);
+        	this.groupBox1.PerformLayout();
+        	this.panel1.ResumeLayout(false);
+        	this.ResumeLayout(false);
 
         }
 
