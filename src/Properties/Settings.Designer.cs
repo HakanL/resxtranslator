d36a6f76f00d95e7589c0b6af061c009ca79b4ec
Marcin Szeniak <marc200333@gmail.com>
2017-01-25 19:44:24 +0100

    Fixed CellEditorShowWhitespace setting having wrong provider and roaming values

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -215,8 +215,10 @@ namespace ResxTranslator.Properties {
         }
         
         [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.SettingsProviderAttribute(typeof(PortableSettingsProvider.PortableSettingsProvider))]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
         public bool CellEditorShowWhitespace {
             get {
                 return ((bool)(this["CellEditorShowWhitespace"]));
