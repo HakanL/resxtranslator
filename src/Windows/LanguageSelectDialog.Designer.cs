d752663d4fb4bd57c7e5e772639bbeb6ba317ef0
Klocman <marc200333@gmail.com>
2020-03-02 00:43:03 +0100

    Fix language select window being resizable

--- src/Windows/LanguageSelectDialog.Designer.cs
+++ src/Windows/LanguageSelectDialog.Designer.cs
@@ -28,67 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-        	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LanguageSelectDialog));
-        	this.button1 = new System.Windows.Forms.Button();
-        	this.button2 = new System.Windows.Forms.Button();
-        	this.comboBox1 = new System.Windows.Forms.ComboBox();
-        	this.panel1 = new System.Windows.Forms.Panel();
-        	this.checkBox1 = new System.Windows.Forms.CheckBox();
-        	this.panel1.SuspendLayout();
-        	this.SuspendLayout();
-        	// 
-        	// button1
-        	// 
-        	resources.ApplyResources(this.button1, "button1");
-        	this.button1.Name = "button1";
-        	this.button1.UseVisualStyleBackColor = true;
-        	this.button1.Click += new System.EventHandler(this.button1_Click);
-        	// 
-        	// button2
-        	// 
-        	this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-        	resources.ApplyResources(this.button2, "button2");
-        	this.button2.Name = "button2";
-        	this.button2.UseVisualStyleBackColor = true;
-        	// 
-        	// comboBox1
-        	// 
-        	resources.ApplyResources(this.comboBox1, "comboBox1");
-        	this.comboBox1.FormattingEnabled = true;
-        	this.comboBox1.Name = "comboBox1";
-        	this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-        	// 
-        	// panel1
-        	// 
-        	resources.ApplyResources(this.panel1, "panel1");
-        	this.panel1.Controls.Add(this.checkBox1);
-        	this.panel1.Controls.Add(this.button2);
-        	this.panel1.Controls.Add(this.comboBox1);
-        	this.panel1.Controls.Add(this.button1);
-        	this.panel1.Name = "panel1";
-        	// 
-        	// checkBox1
-        	// 
-        	resources.ApplyResources(this.checkBox1, "checkBox1");
-        	this.checkBox1.Checked = true;
-        	this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-        	this.checkBox1.Name = "checkBox1";
-        	this.checkBox1.UseVisualStyleBackColor = true;
-        	this.checkBox1.CheckedChanged += new System.EventHandler(this.UpdateComboboxItems);
-        	// 
-        	// LanguageSelectDialog
-        	// 
-        	this.AcceptButton = this.button1;
-        	resources.ApplyResources(this, "$this");
-        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        	this.CancelButton = this.button2;
-        	this.Controls.Add(this.panel1);
-        	this.Name = "LanguageSelectDialog";
-        	this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.LanguageSelectDialog_FormClosed);
-        	this.panel1.ResumeLayout(false);
-        	this.panel1.PerformLayout();
-        	this.ResumeLayout(false);
-        	this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LanguageSelectDialog));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Name = "panel1";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.UpdateComboboxItems);
+            // 
+            // LanguageSelectDialog
+            // 
+            this.AcceptButton = this.button1;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LanguageSelectDialog";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.LanguageSelectDialog_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
