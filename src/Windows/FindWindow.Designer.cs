01dd44054cd20f102eee6ac8dfd23805cb257510
Klocman <14913904+Klocman@users.noreply.github.com>
2022-05-24 09:04:11 +0200

    Remake find window layout to reflow properly #63

--- src/Windows/FindWindow.Designer.cs
+++ src/Windows/FindWindow.Designer.cs
@@ -28,175 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-        	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FindWindow));
-        	this.textBoxSearch = new System.Windows.Forms.TextBox();
-        	this.radioButton1 = new System.Windows.Forms.RadioButton();
-        	this.radioButtonRegexp = new System.Windows.Forms.RadioButton();
-        	this.groupBox1 = new System.Windows.Forms.GroupBox();
-        	this.radioButtonText = new System.Windows.Forms.RadioButton();
-        	this.radioButton4 = new System.Windows.Forms.RadioButton();
-        	this.groupBox2 = new System.Windows.Forms.GroupBox();
-        	this.groupBox3 = new System.Windows.Forms.GroupBox();
-        	this.checkBoxText = new System.Windows.Forms.CheckBox();
-        	this.checkBoxKey = new System.Windows.Forms.CheckBox();
-        	this.checkBoxFile = new System.Windows.Forms.CheckBox();
-        	this.checkBoxLang = new System.Windows.Forms.CheckBox();
-        	this.groupBox4 = new System.Windows.Forms.GroupBox();
-        	this.checkBoxWord = new System.Windows.Forms.CheckBox();
-        	this.checkBoxCS = new System.Windows.Forms.CheckBox();
-        	this.buttonFind = new System.Windows.Forms.Button();
-        	this.button1 = new System.Windows.Forms.Button();
-        	this.groupBox1.SuspendLayout();
-        	this.groupBox2.SuspendLayout();
-        	this.groupBox3.SuspendLayout();
-        	this.groupBox4.SuspendLayout();
-        	this.SuspendLayout();
-        	// 
-        	// textBoxSearch
-        	// 
-        	resources.ApplyResources(this.textBoxSearch, "textBoxSearch");
-        	this.textBoxSearch.Name = "textBoxSearch";
-        	// 
-        	// radioButton1
-        	// 
-        	resources.ApplyResources(this.radioButton1, "radioButton1");
-        	this.radioButton1.Name = "radioButton1";
-        	this.radioButton1.UseVisualStyleBackColor = true;
-        	// 
-        	// radioButtonRegexp
-        	// 
-        	resources.ApplyResources(this.radioButtonRegexp, "radioButtonRegexp");
-        	this.radioButtonRegexp.Name = "radioButtonRegexp";
-        	this.radioButtonRegexp.UseVisualStyleBackColor = true;
-        	// 
-        	// groupBox1
-        	// 
-        	this.groupBox1.Controls.Add(this.radioButtonText);
-        	this.groupBox1.Controls.Add(this.radioButton1);
-        	this.groupBox1.Controls.Add(this.radioButtonRegexp);
-        	this.groupBox1.Controls.Add(this.radioButton4);
-        	resources.ApplyResources(this.groupBox1, "groupBox1");
-        	this.groupBox1.Name = "groupBox1";
-        	this.groupBox1.TabStop = false;
-        	// 
-        	// radioButtonText
-        	// 
-        	resources.ApplyResources(this.radioButtonText, "radioButtonText");
-        	this.radioButtonText.Checked = true;
-        	this.radioButtonText.Name = "radioButtonText";
-        	this.radioButtonText.TabStop = true;
-        	this.radioButtonText.UseVisualStyleBackColor = true;
-        	// 
-        	// radioButton4
-        	// 
-        	resources.ApplyResources(this.radioButton4, "radioButton4");
-        	this.radioButton4.Name = "radioButton4";
-        	this.radioButton4.UseVisualStyleBackColor = true;
-        	// 
-        	// groupBox2
-        	// 
-        	this.groupBox2.Controls.Add(this.textBoxSearch);
-        	resources.ApplyResources(this.groupBox2, "groupBox2");
-        	this.groupBox2.Name = "groupBox2";
-        	this.groupBox2.TabStop = false;
-        	// 
-        	// groupBox3
-        	// 
-        	this.groupBox3.Controls.Add(this.checkBoxText);
-        	this.groupBox3.Controls.Add(this.checkBoxKey);
-        	this.groupBox3.Controls.Add(this.checkBoxFile);
-        	this.groupBox3.Controls.Add(this.checkBoxLang);
-        	resources.ApplyResources(this.groupBox3, "groupBox3");
-        	this.groupBox3.Name = "groupBox3";
-        	this.groupBox3.TabStop = false;
-        	// 
-        	// checkBoxText
-        	// 
-        	resources.ApplyResources(this.checkBoxText, "checkBoxText");
-        	this.checkBoxText.Checked = true;
-        	this.checkBoxText.CheckState = System.Windows.Forms.CheckState.Checked;
-        	this.checkBoxText.Name = "checkBoxText";
-        	this.checkBoxText.UseVisualStyleBackColor = true;
-        	// 
-        	// checkBoxKey
-        	// 
-        	resources.ApplyResources(this.checkBoxKey, "checkBoxKey");
-        	this.checkBoxKey.Name = "checkBoxKey";
-        	this.checkBoxKey.UseVisualStyleBackColor = true;
-        	// 
-        	// checkBoxFile
-        	// 
-        	resources.ApplyResources(this.checkBoxFile, "checkBoxFile");
-        	this.checkBoxFile.Name = "checkBoxFile";
-        	this.checkBoxFile.UseVisualStyleBackColor = true;
-        	// 
-        	// checkBoxLang
-        	// 
-        	resources.ApplyResources(this.checkBoxLang, "checkBoxLang");
-        	this.checkBoxLang.Name = "checkBoxLang";
-        	this.checkBoxLang.UseVisualStyleBackColor = true;
-        	// 
-        	// groupBox4
-        	// 
-        	this.groupBox4.Controls.Add(this.checkBoxWord);
-        	this.groupBox4.Controls.Add(this.checkBoxCS);
-        	resources.ApplyResources(this.groupBox4, "groupBox4");
-        	this.groupBox4.Name = "groupBox4";
-        	this.groupBox4.TabStop = false;
-        	// 
-        	// checkBoxWord
-        	// 
-        	resources.ApplyResources(this.checkBoxWord, "checkBoxWord");
-        	this.checkBoxWord.Name = "checkBoxWord";
-        	this.checkBoxWord.UseVisualStyleBackColor = true;
-        	// 
-        	// checkBoxCS
-        	// 
-        	resources.ApplyResources(this.checkBoxCS, "checkBoxCS");
-        	this.checkBoxCS.Name = "checkBoxCS";
-        	this.checkBoxCS.UseVisualStyleBackColor = true;
-        	// 
-        	// buttonFind
-        	// 
-        	resources.ApplyResources(this.buttonFind, "buttonFind");
-        	this.buttonFind.Name = "buttonFind";
-        	this.buttonFind.UseVisualStyleBackColor = true;
-        	this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
-        	// 
-        	// button1
-        	// 
-        	this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-        	resources.ApplyResources(this.button1, "button1");
-        	this.button1.Name = "button1";
-        	this.button1.UseVisualStyleBackColor = true;
-        	// 
-        	// FindWindow
-        	// 
-        	this.AcceptButton = this.buttonFind;
-        	resources.ApplyResources(this, "$this");
-        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        	this.CancelButton = this.button1;
-        	this.Controls.Add(this.button1);
-        	this.Controls.Add(this.buttonFind);
-        	this.Controls.Add(this.groupBox2);
-        	this.Controls.Add(this.groupBox3);
-        	this.Controls.Add(this.groupBox4);
-        	this.Controls.Add(this.groupBox1);
-        	this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-        	this.MaximizeBox = false;
-        	this.MinimizeBox = false;
-        	this.Name = "FindWindow";
-        	this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-        	this.Load += new System.EventHandler(this.FindDialog_Load);
-        	this.groupBox1.ResumeLayout(false);
-        	this.groupBox1.PerformLayout();
-        	this.groupBox2.ResumeLayout(false);
-        	this.groupBox2.PerformLayout();
-        	this.groupBox3.ResumeLayout(false);
-        	this.groupBox3.PerformLayout();
-        	this.groupBox4.ResumeLayout(false);
-        	this.groupBox4.PerformLayout();
-        	this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FindWindow));
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonRegexp = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBoxText = new System.Windows.Forms.CheckBox();
+            this.checkBoxKey = new System.Windows.Forms.CheckBox();
+            this.checkBoxFile = new System.Windows.Forms.CheckBox();
+            this.checkBoxLang = new System.Windows.Forms.CheckBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.checkBoxWord = new System.Windows.Forms.CheckBox();
+            this.checkBoxCS = new System.Windows.Forms.CheckBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxSearch
+            // 
+            resources.ApplyResources(this.textBoxSearch, "textBoxSearch");
+            this.textBoxSearch.Name = "textBoxSearch";
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRegexp
+            // 
+            resources.ApplyResources(this.radioButtonRegexp, "radioButtonRegexp");
+            this.radioButtonRegexp.Name = "radioButtonRegexp";
+            this.radioButtonRegexp.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.flowLayoutPanel3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Controls.Add(this.textBoxSearch);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Controls.Add(this.flowLayoutPanel4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // checkBoxText
+            // 
+            resources.ApplyResources(this.checkBoxText, "checkBoxText");
+            this.checkBoxText.Checked = true;
+            this.checkBoxText.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxText.Name = "checkBoxText";
+            this.checkBoxText.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKey
+            // 
+            resources.ApplyResources(this.checkBoxKey, "checkBoxKey");
+            this.checkBoxKey.Name = "checkBoxKey";
+            this.checkBoxKey.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFile
+            // 
+            resources.ApplyResources(this.checkBoxFile, "checkBoxFile");
+            this.checkBoxFile.Name = "checkBoxFile";
+            this.checkBoxFile.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLang
+            // 
+            resources.ApplyResources(this.checkBoxLang, "checkBoxLang");
+            this.checkBoxLang.Name = "checkBoxLang";
+            this.checkBoxLang.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            resources.ApplyResources(this.groupBox4, "groupBox4");
+            this.groupBox4.Controls.Add(this.flowLayoutPanel2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.TabStop = false;
+            // 
+            // checkBoxWord
+            // 
+            resources.ApplyResources(this.checkBoxWord, "checkBoxWord");
+            this.checkBoxWord.Name = "checkBoxWord";
+            this.checkBoxWord.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCS
+            // 
+            resources.ApplyResources(this.checkBoxCS, "checkBoxCS");
+            this.checkBoxCS.Name = "checkBoxCS";
+            this.checkBoxCS.UseVisualStyleBackColor = true;
+            // 
+            // buttonFind
+            // 
+            resources.ApplyResources(this.buttonFind, "buttonFind");
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            resources.ApplyResources(this.flowLayoutPanel1, "flowLayoutPanel1");
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.buttonFind);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            // 
+            // flowLayoutPanel2
+            // 
+            resources.ApplyResources(this.flowLayoutPanel2, "flowLayoutPanel2");
+            this.flowLayoutPanel2.Controls.Add(this.checkBoxCS);
+            this.flowLayoutPanel2.Controls.Add(this.checkBoxWord);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            // 
+            // flowLayoutPanel3
+            // 
+            resources.ApplyResources(this.flowLayoutPanel3, "flowLayoutPanel3");
+            this.flowLayoutPanel3.Controls.Add(this.radioButton1);
+            this.flowLayoutPanel3.Controls.Add(this.radioButtonRegexp);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            // 
+            // flowLayoutPanel4
+            // 
+            resources.ApplyResources(this.flowLayoutPanel4, "flowLayoutPanel4");
+            this.flowLayoutPanel4.Controls.Add(this.checkBoxText);
+            this.flowLayoutPanel4.Controls.Add(this.checkBoxKey);
+            this.flowLayoutPanel4.Controls.Add(this.checkBoxLang);
+            this.flowLayoutPanel4.Controls.Add(this.checkBoxFile);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            // 
+            // FindWindow
+            // 
+            this.AcceptButton = this.buttonFind;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FindWindow";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Load += new System.EventHandler(this.FindDialog_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -206,8 +228,6 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButtonRegexp;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButtonText;
-        private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.CheckBox checkBoxText;
@@ -219,5 +239,9 @@
         private System.Windows.Forms.Button buttonFind;
         private System.Windows.Forms.CheckBox checkBoxFile;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
